5065a69e781229c2d7a7f581b6feee37e307592d
chusnaval <chusnaval@gmail.com>
2025-11-09 15:52:31 +0100

    Update DataSourceConfig.java

--- lugus/src/main/java/lugus/config/DataSourceConfig.java
+++ lugus/src/main/java/lugus/config/DataSourceConfig.java
@@ -12,6 +12,6 @@ public class DataSourceConfig {
 	@Bean
 	public DataSource getDataSource() {
 		return DataSourceBuilder.create().driverClassName("org.postgresql.Driver")
-				.url("jdbc:postgresql://localhost:5432/lugus_dev").username("lugus_usr").password("lugus_pass").build();
+				.url("jdbc:postgresql://192.168.1.61:5432/lugus?currentSchema=lugus").username("lugus_usr").password("lugusNov2015").build();
 	}
 }
