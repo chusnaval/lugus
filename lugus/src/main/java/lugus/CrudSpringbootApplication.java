d91a2bd52e4a8af5a0ce8e7bd6a4447c44727e00
chusnaval <chusnaval@gmail.com>
2025-11-18 07:11:09 +0100

    War depleyment
    We enabled war deployment and fix applications.properties been excluded in deployments

--- lugus/src/main/java/lugus/CrudSpringbootApplication.java
+++ lugus/src/main/java/lugus/CrudSpringbootApplication.java
@@ -2,12 +2,25 @@ package lugus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrudSpringbootApplication {
+public class CrudSpringbootApplication  extends SpringBootServletInitializer {
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return customizerBuilder(builder);
+	}
+	
 	public static void main(String[] args) {
 		   SpringApplication.run(CrudSpringbootApplication.class, args);
 	}
 
+
+	private static SpringApplicationBuilder customizerBuilder(SpringApplicationBuilder builder) {
+		return builder.sources(CrudSpringbootApplication.class).bannerMode(org.springframework.boot.Banner.Mode.OFF);
+	}
+	
 }
