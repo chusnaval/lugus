f2e6b3ce13bc77afb0c4fb58e4bbf5d099168347
chusnaval <chusnaval@gmail.com>
2026-01-18 20:00:11 +0100

    Add getTitle method to GroupFilms and update templates
    Introduces a getTitle() method in GroupFilms to return the film title from either Pelicula or ImdbTitleBasics. Updates groups/detail.html to use f.title instead of f.itb.originaltitle for displaying the title. Adds a horizontal rule in peliculas/detail.html for improved UI separation.

--- lugus/src/main/java/lugus/model/groups/GroupFilms.java
+++ lugus/src/main/java/lugus/model/groups/GroupFilms.java
@@ -16,7 +16,7 @@ import lugus.model.imdb.ImdbTitleBasics;
 
 @Entity
 @Table(name = "grupos_peliculas")
-@Data 
+@Data
 @EqualsAndHashCode(of = "id")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,21 +24,31 @@ public class GroupFilms {
 
 	@Id
 	private int id;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY, optional = true)
-	@JoinColumn(name = "grupo_id", nullable = true) 
+	@JoinColumn(name = "grupo_id", nullable = true)
 	private Group group;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY, optional = true)
 	@JoinColumn(name = "peliculas_id", nullable = true)
 	private Pelicula pelicula;
-	
+
 	@Column(nullable = false)
 	private int orden;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY, optional = true)
 	@JoinColumn(nullable = false, name = "imdb_id")
 	private ImdbTitleBasics itb;
-	
-	
+
+	public String getTitle() {
+		String titulo = "";
+		if (pelicula != null) {
+			titulo = pelicula.getTitulo();
+		}else {
+			titulo = itb.getPrimarytitle();
+		}
+
+		return titulo;
+	}
+
 }
