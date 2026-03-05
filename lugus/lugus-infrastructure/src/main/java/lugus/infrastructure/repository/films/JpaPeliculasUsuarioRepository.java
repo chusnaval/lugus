253ec27f0595a2f709163ccb96d1d50e574ae206
chusnaval <chusnaval@gmail.com>
2026-03-05 21:30:15 +0100

    Update JpaPeliculasUsuarioRepository.java

--- lugus/lugus-infrastructure/src/main/java/lugus/infrastructure/repository/films/JpaPeliculasUsuarioRepository.java
+++ lugus/lugus-infrastructure/src/main/java/lugus/infrastructure/repository/films/JpaPeliculasUsuarioRepository.java
@@ -2,11 +2,11 @@ package lugus.infrastructure.repository.films;
 
 
 
-import lugus.repository.user.PeliculasUsuarioRepository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface JpaPeliculasUsuarioRepository
-        extends PeliculasUsuarioRepository, JpaRepository<lugus.model.films.PeliculasUsuario, Long> {
+        extends lugus.repository.films.PeliculasUsuarioRepository, JpaRepository<lugus.model.films.PeliculasUsuario, Long> {
 
 }
